a51723203c6bc076612b05c063181f6e62ca27f8
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-17 15:28:22 +0200

    Initialize database and viewModel via magic. Dependency injection shall be reviewed.

--- app/src/main/java/abs/apps/room_database/MainActivity.kt
+++ app/src/main/java/abs/apps/room_database/MainActivity.kt
@@ -1,46 +1,43 @@
 package abs.apps.room_database
 
+import ContactScreen
+import abs.apps.room_database.ui.theme.Room_databaseTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import abs.apps.room_database.ui.theme.Room_databaseTheme
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 
 class MainActivity : ComponentActivity() {
+
+    // Bad way to initialize database: Learn about Dependency Injection in Android ... GOT C#
+    private val dataBase by lazy {
+        Room.databaseBuilder(applicationContext, ContactDatabase::class.java, "contacts.db").build()
+    }
+
+    // Bad way to initialize viewModel: Learn about Dependency Injection in Android ... GOT C#
+    private val viewModel by viewModels<ContactViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ContactViewModel(dataBase.dao) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Room_databaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val state by viewModel.state.collectAsState()
+                ContactScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Room_databaseTheme {
-        Greeting("Android")
-    }
-}
